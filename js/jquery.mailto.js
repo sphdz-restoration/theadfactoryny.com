1e7e89ee6e34df4048030b6f0e61f468572dd184
Declan Hoare <declanhoare@exemail.com.au>
2019-06-29 23:40:29 +1000

    remove javascript + rename image to work around adblock error

--- js/jquery.mailto.js
+++
@@ -1,8 +0,0 @@
-/* safer mailto link */
-
-jQuery.fn.mailto = function() {
-	return this.each(function(){
-		var email = $(this).attr('href').replace(/\s*\(.+\)\s*/, "@");
-		$(this).attr('href', 'mailto:'+email+'');
-	});
-};
